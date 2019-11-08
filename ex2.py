d4b486b1de21eeb941c533861fd525352ea560a2
karaeval <56735453+karaeval@users.noreply.github.com>
2019-11-08 08:46:05 +0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# Даны действительные числа А, В, С. Найти максимальное и минимальное из этих чисел.
+number1 = input("Введите действительное число 1 ")
+number1 = int(number1)
+number2 = input("Введите действительное число 2 ")
+number2 = int(number2)
+number3 = input("Введите действительное число 3 ")
+number3 = int(number3)
+
+print(max(number1, number2, number3))
+print(min(number1, number2, number3))
